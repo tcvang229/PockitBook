8ae95978ac089f15e9b548f762aa91381ba7ef73
tcvang229 <tswjfwmeng@gmail.com>
2024-08-30 00:02:45 -0500

    first commit

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace PockitBook.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace PockitBook.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
